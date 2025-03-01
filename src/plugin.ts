267046ab87b4046fd4f68abec164a8016debb436
believelody <believelody@gmail.com>
2025-03-01 18:33:43 +0100

    init plugin file

--- src/plugin.ts
+++ src/plugin.ts
@@ -6,11 +6,13 @@ import fastifyUnderPressure from '@fastify/under-pressure';
 import { Kita } from '@kitajs/runtime';
 import closeWithGrace from 'close-with-grace';
 import fp from 'fastify-plugin';
-import path from 'path';
+import path from 'node:path';
 
 export default fp(async (app) => {
+  // Registers the generated kita plugin
   app.register(Kita);
 
+  // Measures process load with automatic handling of "Service Unavailable"
   app.register(fastifyUnderPressure, {
     maxEventLoopDelay: 1000,
     maxHeapUsedBytes: 1000000000,
@@ -18,16 +20,23 @@ export default fp(async (app) => {
     maxEventLoopUtilization: 0.98
   });
 
+  // Important security headers for Fastify
   app.register(fastifyHelmet, {
     global: true,
+    // The Suspense component uses inline <script> tags
     contentSecurityPolicy: false
   });
 
-  // console.log({ path })
+  // Serves static files from the public directory
   app.register(fastifyStatic, {
     root: path.resolve('public')
   });
 
+  // Add your custom stuff here
+  // app.register(myPlugin)
+  // ...
+
+  // Delay is the number of milliseconds for the graceful close to finish
   const closeListeners = closeWithGrace({ delay: 500 }, async ({ err }) => {
     if (err) {
       app.log.error(err);
